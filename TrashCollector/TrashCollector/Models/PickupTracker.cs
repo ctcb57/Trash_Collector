4b3a902cd3fce3cde2304ff5d7c52bf948ce9440
Charles Clark <ctcb57@gmail.com>
2019-09-08 23:03:40 -0500

    Create pickup tracker model in order to log pickups when they are confirmed by an employee

---
+++ TrashCollector/TrashCollector/Models/PickupTracker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class PickupTracker
+    {
+        [Key]
+        public int pickupId { get; set; }
+
+        [Display(Name ="Pickup Date")]
+        [DataType(DataType.Date)]
+        public DateTime? pickupDate { get; set; }
+
+        [ForeignKey("Employee")]
+        public int employeeId { get; set; }
+        public Employee Employee { get; set; }
+
+        [ForeignKey("Customer")]
+        public int customerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
